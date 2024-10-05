bad88a3bf0d06f443bac982a993494b74cc9694d
giridharchalla2k8 <giridharchalla2k8@gmail.com>
2024-10-05 19:08:59 +0530

    added 3 models

--- mlmodel.py
+++ mlmodel.py
@@ -1,1 +1,16 @@
 print ("ML model files are added here")
+
+def model11:
+    print ("model1")
+
+model11
+
+def model12:
+    print ("model1")
+
+model12
+
+def model13:
+    print ("model1")
+
+model13
